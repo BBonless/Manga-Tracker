da87071493c44bbb756f99a98debd8f1a01ff6df
BBonless <50674795+BBonless@users.noreply.github.com>
2020-02-07 17:23:51 +0100

    Add files via upload

---
+++ ChooseDay.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MangaTracker
+{
+    /// <summary>
+    /// Interaction logic for ChooseDay.xaml
+    /// </summary>
+    public partial class ChooseDay : Window
+    {
+        public ChooseDay()
+        {
+            InitializeComponent();
+        }
+        public int day { get; private set; }
+        private void Monday(object sender, RoutedEventArgs e)
+        {
+            day = 1;
+            DayLabel.Content = "Monday";
+
+        }
+        private void Tuesday(object sender, RoutedEventArgs e)
+        {
+            day = 2;
+            DayLabel.Content = "Tuesday";
+
+        }
+        private void Wednesday(object sender, RoutedEventArgs e)
+        {
+            day = 3;
+            DayLabel.Content = "Wednesday";
+
+        }
+        private void Thursday(object sender, RoutedEventArgs e)
+        {
+            day = 4;
+            DayLabel.Content = "Thursday";
+
+        }
+        private void Friday(object sender, RoutedEventArgs e)
+        {
+            day = 5;
+            DayLabel.Content = "Friday";
+
+        }
+        private void Saturday(object sender, RoutedEventArgs e)
+        {
+            day = 6;
+            DayLabel.Content = "Saturday";
+
+        }
+        private void Sunday(object sender, RoutedEventArgs e)
+        {
+            
+            day = 7;
+            DayLabel.Content = "Sunday";
+
+        }
+        public int Confirm(object sender, RoutedEventArgs e)
+        {
+            return day; 
+        }
+    }
+}
---
+++ NewEntry.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MangaTracker
+{
+    /// <summary>
+    /// Interaction logic for NewEntry.xaml
+    /// </summary>
+    public partial class NewEntry : Window
+    {
+        public string Name;
+        public int Day;
+
+        public NewEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            NameOfManga.Text = Convert.ToString(Day);
+
+        }
+
+        private void ChooseDay(object sender, RoutedEventArgs e)
+        {
+            ChooseDay CD = new ChooseDay();
+            CD.ShowDialog();
+            Day = CD.Confirm();
+            NameOfManga.Text = Convert.ToString(Day);
+            
+
+        }
+    }
+}
